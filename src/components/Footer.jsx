5d7f5a5146990d31d8612d4cab0e0490cd8c579c
jayzumen <reinhardtjanniklas96@gmail.com>
2022-06-25 15:58:03 +0200

    added resource of API

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -110,7 +110,20 @@ const Footer = () => {
       </div>
       <div>
         <p className="text-gray-400 mt-4">
-          <small>© 2022, made by Jan-Niklas Reinhardt</small>
+          <small>© 2022, made by Jan-Niklas Reinhardt </small>
+          || API from
+          <a
+            href="https://www.themoviedb.org/"
+            title="themoviedb"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="h-5 w-25 inline ml-2 mb-1"
+              src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
+              alt="themoviedb logo"
+            />
+          </a>
         </p>
       </div>
     </div>
