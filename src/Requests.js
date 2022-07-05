80bb414cfb44b2e3a57fc1d742f02e00fd92861d
jayzumen <reinhardtjanniklas96@gmail.com>
2022-07-05 12:20:55 +0200

    w

--- src/Requests.js
+++ src/Requests.js
@@ -1,9 +1,11 @@
+const key = process.env.REACT_APP_IMDB_API_KEY;
+
 const requests = {
-  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US&page=1`,
-  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US&page=1`,
-  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US&page=2`,
-  requestCrime: `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US&query=crime&page=1`,
-  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US&page=1`,
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestCrime: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=crime&page=1`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
 };
 
 export default requests;
